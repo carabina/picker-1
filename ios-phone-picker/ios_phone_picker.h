1c36ab9999b30865d1b51ebf293e2cf92be6ac27
manuelvrhovac <manuel.vrhovac@icloud.com>
2017-12-05 15:30:02 +0100

    Initial Commit

---
+++ ios-phone-picker/ios_phone_picker.h
@@ -0,0 +1,19 @@
+//
+//  ios_phone_picker.h
+//  ios-phone-picker
+//
+//  Created by Manuel Vrhovac on 05/12/2017.
+//  Copyright © 2017 INO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ios_phone_picker.
+FOUNDATION_EXPORT double ios_phone_pickerVersionNumber;
+
+//! Project version string for ios_phone_picker.
+FOUNDATION_EXPORT const unsigned char ios_phone_pickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ios_phone_picker/PublicHeader.h>
+
+
